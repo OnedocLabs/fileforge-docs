36885160b08643f5070d9630657dbbd7e14eac84
Pierre Dorge <pierre.dorge@onedoclabs.com>
2024-04-23 13:52:11 +0200

    Updates docs, bash script

--- fern/scripts/copy_react_print_docs.sh
+++ fern/scripts/copy_react_print_docs.sh
@@ -1,3 +1,5 @@
+rm -rf react-print-pdf
+rm -rf fern/docs.yml
 git clone --single-branch --branch ffo-48-include-documentation-for-the-react-print-library-with https://github.com/OnedocLabs/react-print-pdf.git
 cd react-print-pdf
 cp ./docs/docs.yml ../fern/docs.yml
