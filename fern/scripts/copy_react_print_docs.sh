b7f1078bc94db0a29d9db62c3e70c2a523c9e3be
Pierre Dorge <pierre.dorge@onedoclabs.com>
2024-05-09 17:28:17 +0200

    Upgrades fern and fern overrides

--- fern/scripts/copy_react_print_docs.sh
+++ fern/scripts/copy_react_print_docs.sh
@@ -1,5 +1,6 @@
-rm -rf react-print-pdf
+rm -rf fern/react-print-pdf
 rm -rf fern/docs.yml
+cd fern
 git clone --single-branch --branch ffo-48-include-documentation-for-the-react-print-library-with https://github.com/OnedocLabs/react-print-pdf.git
 cd react-print-pdf
-cp ./docs/docs.yml ../fern/docs.yml
+cp ./docs/docs.yml ../docs.yml
