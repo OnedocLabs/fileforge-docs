a9ff593a99fbca8f6b4a33a8a1ceb97d4d93fa3d
Titouan Launay <titouan.launay@lleedpartners.com>
2024-06-04 17:17:38 +0200

    Disables copying of react-print docs.yml

--- fern/scripts/copy_react_print_docs.sh
+++ fern/scripts/copy_react_print_docs.sh
@@ -3,4 +3,4 @@ rm -rf fern/docs.yml
 cd fern
 git clone --single-branch --branch ffo-48-include-documentation-for-the-react-print-library-with https://github.com/OnedocLabs/react-print-pdf.git
 cd react-print-pdf
-cp ./docs/docs.yml ../docs.yml
+# cp ./docs/docs.yml ../docs.yml
